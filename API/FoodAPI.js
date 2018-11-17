fec5a2c9efa9084d8b2c5acc2ce96ea562f86340
Dan-RT <daniel.regnard@efrei.net>
2018-11-17 17:12:56 -0500

    creation of recipes with dynamic keywords retrieval

--- API/FoodAPI.js
+++ API/FoodAPI.js
@@ -1,5 +1,5 @@
 export function getRecipesFromApiWithSearchedText (food) {
-  const url = 'http://10.0.2.2:3000/recipes/search/' + food
+  const url = 'http://10.0.2.2:3000/recipes/search/keyword/' + food
 
   console.log("getRecipesFromApiWithSearchedText API call to : " + url);
 
@@ -14,7 +14,7 @@ export function getRecipesFromApiWithSearchedText (food) {
 }
 
 export function getRecipesFromApibyId (id) {
-  const url = 'http://10.0.2.2:3000/recipes/recipe/id/' + id
+  const url = 'http://10.0.2.2:3000/recipes/search/id/' + id
 
   console.log("getRecipesFromApibyId API call to : " + url);
 
@@ -50,7 +50,22 @@ export function postNewRecipe() {
 
 }
 
+
+export function deleteRecipeByID(id) {
+  //pas encore utilisée
+  const url = 'http://10.0.2.2:3000/recipes/delete/id/' + id
+
+  console.log("deleteRecipeByID API call to : " + url);
+
+  return fetch(url).then(function(response){
+    return response.json();
+  }).catch(function(error) {
+    throw error;
+  });
+}
+
 export function getDeleteRecipe() {
+  //à delete
   const url = 'http://10.0.2.2:3000/recipes/delete'
 
   console.log("_postDeleteRecipe API call to : " + url);
